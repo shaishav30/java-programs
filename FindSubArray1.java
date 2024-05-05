ce11e4abaf03769cff1eb95fefd53f7c81b70b10
Shaishav <shaishav_shah2002@yahoo.com>
2024-05-05 10:35:12 +0530

    Revert "added optimised function"
    This reverts commit 855ee726a0e0e1a400a9f539b4b29752cd9b8412.

--- FindSubArray1.java
+++ FindSubArray1.java
@@ -3,52 +3,10 @@ public class FindSubArray1 {
 
     public static void main(String[] args) {
         int arr[] = {10,3,5,8,6,12,20,11,31};
-        int sum = 31;
 
-        //findSubArray(arr,sum);
-        findSubArrayBestSolution(arr,sum);
-
-        
-        System.out.println("done");
-    }
-
-    private static void findSubArrayBestSolution(int arr[], int sum){
-        int i=0;
-        int j=0;
-        int tmpSum = arr[0];
-       
-
-        while(i < arr.length && j < arr.length){
-            if(tmpSum == sum){
-                System.err.println("output - "+i+", "+j);
-                //break;
-                if(i == arr.length-1)
-                    break;
-
-                i++;
-                j=i;
-                tmpSum=arr[i];
-           } else if(tmpSum < sum){
-                
-                    j++;
-                    tmpSum += arr[j];
-                
-            } else if(tmpSum > sum) {
-                
-                    tmpSum -= arr[i];
-                    i++;
-                
-            } else if(i == arr.length && j == arr.length)
-                break;
-
-        }
-
-    }
-
-    private static void findSubArray(int arr[], int sum){
         int i=0;
         int j=1;
-        
+        int sum = 31;
         int tmpSum;
 
         while(i<arr.length){
@@ -71,5 +29,6 @@ public class FindSubArray1 {
             }
             i++;
         }
+        System.out.println("done");
     }
 }
